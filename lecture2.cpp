bcf73a6964b3f470d2d12d1f19f00c664b867613
Srijal <srijal0901@gmail.com>
2023-11-09 13:10:40 +0530

    lec3

--- lecture2.cpp
+++ lecture2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 // int main () {
-    int n ;
+    // int n ;
 // cin>>n;
 // n = cin.get();
 // if (n>=0) {
@@ -10,10 +10,24 @@ using namespace std;
 // else {cout << "Number is negative" << endl;
 // }
 // }
-int main(){
-    int a, b;
-    cout<< "Enter the value of a:" << endl;
-    cin>>a;
-    cout<< "Enter the value of b:" << endl;
-    cin>>b;
+// int main(){
+//     int a, b;
+//     cout<< "Enter the value of a:" << endl;
+//     cin>>a;
+//     cout<< "Enter the value of b:" << endl;
+//     cin>>b;
+// }
+int main (){
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    if (ch >= 'a'  && ch <= 'z')
+    { cout << "It is in lowercase" << endl;
+    }
+    else if (ch >= 'A'  && ch <= 'Z' )
+    {cout << "It is upper case" << endl; }
+    else if ( ch >= '1' && ch <= '9' )
+    {cout << "It is number" << endl; }
+    else { cout << "It is neither lower case, upper case or number";
+    };
 }
