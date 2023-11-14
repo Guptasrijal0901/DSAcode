601f6679f9898ac85ad267f336c26a2a18ceb170
Srijal <srijal0901@gmail.com>
2023-11-14 14:01:11 +0530

    lecture3

--- lecture2.cpp
+++ lecture2.cpp
@@ -17,17 +17,105 @@ using namespace std;
 //     cout<< "Enter the value of b:" << endl;
 //     cin>>b;
 // }
+// int main (){
+//     char ch;
+//     cout << "Enter a character: ";
+//     cin >> ch;
+//     if (ch >= 'a'  && ch <= 'z')
+//     { cout << "It is in lowercase" << endl;
+//     }
+//     else if (ch >= 'A'  && ch <= 'Z' )
+//     {cout << "It is upper case" << endl; }
+//     else if ( ch >= '1' && ch <= '9' )
+//     {cout << "It is number" << endl; }
+//     else { cout << "It is neither lower case, upper case or number";
+//     };
+// }
+// int main (){
+//     int n ;
+//     cout << "Enter your n : ";
+//     cin >> n;
+//     int i = 1;
+//     while (i <= n){
+//         cout << i << " " ;
+//         i = i + 1;
+//     }
+// }
+// int main (){
+//     int n;
+//     cout << "Enter your n: ";
+//     cin >> n ;
+//     int i = 1;
+//     int sum = 0;
+//     while (i <= n)
+//     {
+//         sum = sum + i;
+//         i = i + 1;
+//     }
+//     cout << "Value of sum is : " << sum << endl ;
+// }
+// int main (){
+//     int n ;
+//     cout << "Enter n: ";
+//     cin >> n;
+//     int i = 2;
+//     int sum = 0;
+//     while (i <= n){
+//         sum = sum + i ;
+//         i =  i + 2 ;
+//     }
+//     cout << "The sum of even number is : " << sum << endl;
+//     }
+// int main (){
+//     double fahrenheit , celsius;
+//      cout << "Enter temperature in Fahrenheit: ";
+//      cin >> fahrenheit;
+//      celsius = (fahrenheit - 32 )* 5/9;
+//      cout << "Temperature in celsius: " << celsius << endl;
+// }
+// int main (){
+//     int n ;
+//     cout << "Enter your number : ";
+//     cin >> n ;
+//     int i = 2 ;
+//     while (i < n){
+//     if ( n % i == 0){
+//     cout << "Number is not prime for " << i << endl; }
+//     else{ cout << "Number is prime for " << i << endl ;
+//     }
+//     i = i + 1;
+//     }
+// }
+// int main (){
+//     int n ;
+//     cin >> n ;
+//     int row = 1;
+//     while ( row <= n )
+//     {
+//        int col = 1 ;
+//        while (col <= n )
+//        {
+//         cout << "*    ";
+//         col = col + 1  ; 
+//         }
+//         cout << endl;
+//         row = row + 1 ;
+//     }
+// }
+#include <iostream>
+using namespace std;
 int main (){
-    char ch;
-    cout << "Enter a character: ";
-    cin >> ch;
-    if (ch >= 'a'  && ch <= 'z')
-    { cout << "It is in lowercase" << endl;
+int n ;
+cin >> n;
+int i = 1;
+while(i <= 1);
+{
+    int j = 1 ;
+    while ( j <= n ){
+        cout << j ;
+        j = j + 1 ;
     }
-    else if (ch >= 'A'  && ch <= 'Z' )
-    {cout << "It is upper case" << endl; }
-    else if ( ch >= '1' && ch <= '9' )
-    {cout << "It is number" << endl; }
-    else { cout << "It is neither lower case, upper case or number";
-    };
+    cout << endl;
+    i = i + 1 ;
+}
 }
